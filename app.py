97e1004cf10611668f6a0c4578650b12bd3b1e14
Jannes <36892200+Bmbus@users.noreply.github.com>
2020-04-18 11:07:48 +0200

    Add error-handler, routes and more html templates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, redirect, url_for
 from src.dataminer import Dataminer
-from core import Database
+from core import Database, error
 
 app = Flask(__name__)
 db = Database()
@@ -12,11 +12,11 @@ def login():
 
 @app.route("/")
 def index():
-    return render_template("index.html", hum=dm.get_h_t()["humidity"], temp=dm.get_h_t()["temperature"], title="Tomato - Home")
+    return render_template("index.html", hum=dm.get_h_t()["humidity"], temp=dm.get_h_t()["temperature"], title="Tomato - Home", username="Jannes") # TODO get username from Database
 
 @app.route("/graphics")
 def graphics():
-    return "Graphics"
+    return render_template("graphics.html", title="Tomato - Graphics")
 
 @app.route("/config")
 def config():
@@ -25,6 +25,10 @@ def config():
 @app.route("/raw_data")
 def raw_data():
     return render_template("raw_data.html")
+
+@app.route("/log")
+def logging():
+    return
 
 @app.route("/about")
 def about():
@@ -36,6 +40,7 @@ def init():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
+    app.register_blueprint(error)
     try:
         db.setup()
         app.run(debug=True, port=8080, threaded=True, host="0.0.0.0")
